fde2795e7c127222672f2133985f656b2a8d53af
신 우현 <woo_hyun_1@naver.com>
2023-01-03 01:05:10 +0900

    Expansion of styled

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -22,15 +16,18 @@ const Text = styled.span`
   color: white;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 // 구현 영역
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Box bgColor="tomato" />
+      <Circle bgColor="black" />
     </Father>
   );
 }
