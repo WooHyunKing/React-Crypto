7126164bb38b7c6d9f3de0747fe8e30730075c89
신 우현 <woo_hyun_1@naver.com>
2023-01-04 21:08:20 +0900

    Setting of typescript and learn about Typing the Props

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Circle from "./Circle";
+
+// style 영역
+
+// 구현 영역
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
