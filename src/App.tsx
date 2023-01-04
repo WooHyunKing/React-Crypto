e3f1326b6bad5551608535090e7c1d4425aef1c4
신 우현 <woo_hyun_1@naver.com>
2023-01-04 22:55:45 +0900

    Protect state and event by typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import Circle from "./Circle";
-
 // style 영역
+
+import React, { useState } from "react";
 
 // 구현 영역
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    //const value = event.currentTarget.value
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
