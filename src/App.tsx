d07a7a2b36d143cc40a7c94f61b6833d6f33fba9
신 우현 <woo_hyun_1@naver.com>
2023-01-05 21:03:05 +0900

    Learn about how to give a type to theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-// style 영역
-
 import React, { useState } from "react";
+import styled from "styled-components";
 
-// 구현 영역
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    //const value = event.currentTarget.value
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
