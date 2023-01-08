ff1314a2c2598faa29477bef4840721104b6fb9e
신 우현 <woo_hyun_1@naver.com>
2023-01-08 17:55:48 +0900

    Get Data Types from API

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -33,12 +34,103 @@ interface RouteState {
   name: string;
 }
 
+// array는 Object가 아니라 어떤 Object의 배열인지 설명해주어야 함
+
+// interface ITag {
+//   coin_counter: number;
+//   ico_counter: number;
+//   id: string;
+//   name: string;
+// }
+
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  //   array는 Object가 아니라 어떤 Object의 배열인지 설명해주어야 함
+  //   tags: ITag[];
+  //   team: object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const { coinId } = useParams<RouteParams>();
   // Typescript에게 useParams가 문자열 타입인 coinId를 포함하는
   // 객체를 반환할 것이라는 것을 알려주는 것
   const [loading, setLoading] = useState(true);
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+
+  const getInfo = async () => {
+    const response = await axios(
+      `https://api.coinpaprika.com/v1/coins/${coinId}`
+    );
+    console.log(response.data);
+    setInfo(response.data);
+  };
+
+  const getPrice = async () => {
+    const response = await axios(
+      `https://api.coinpaprika.com/v1/tickers/${coinId}`
+    );
+    console.log(response.data);
+    setPriceInfo(response.data);
+  };
+
+  useEffect(() => {
+    getInfo();
+    getPrice();
+  }, []);
 
   return (
     <Container>
