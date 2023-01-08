59c4f37e555ed8e3acfeec3ec38caa010034b5c5
신 우현 <woo_hyun_1@naver.com>
2023-01-09 00:20:00 +0900

    Get price chart by API

--- src/api.ts
+++ src/api.ts
@@ -17,3 +17,12 @@ export const getPrice = async (coinId: string) => {
     .get(`${BASE_URL}/tickers/${coinId}`)
     .then((res) => res.data);
 };
+
+export const getCoinHistory = async (coinId: string) => {
+  //   const endDate = Math.floor(Date.now() / 1000);
+  //   const startDate = endDate - 60 * 60 * 24 * 7;
+
+  return await axios
+    .get(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`)
+    .then((res) => res.data);
+};
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { getCoinHistory } from "../api";
 
-const Chart = () => {
+interface ChartProps {
+  coinId: string;
+}
+
+const Chart = ({ coinId }: ChartProps) => {
+  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+    getCoinHistory(coinId)
+  );
+  console.log(data);
   return <h1>Chart</h1>;
 };
 
